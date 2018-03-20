f64d3189eea7398a5c23edf4afa15ff507f0a117
ALEXIS SUAREZ <jainjo_pluso@comunidad.unam.mx>
2018-03-20 13:43:39 -0600

    Aplicacion commit

---
+++ DistribucionDependencias.Domain/Abstract/IArticuloRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DistribucionDependencias.Domain.Entities;
+
+namespace DistribucionDependencias.Domain.Abstract
+{
+    public interface IArticuloRepository
+    {
+        IEnumerable<Articulo> Articulos { get; }
+    }
+}
---
+++ DistribucionDependencias.Domain/Entities/Articulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistribucionDependencias.Domain.Entities
+{
+    public class Articulo
+    {
+        public string ArticuloID { get; set; }
+        public string Nombre { get; set; }
+        public string NombreCorto { get; set; }
+        public decimal Precio { get; set; }
+        public Categoria categoria { get; set; }
+    }
+}
