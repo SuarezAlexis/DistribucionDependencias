18b2dfde98f0a5ddf6825ef3ef1d6c2a71349108
ALEXIS SUAREZ <jainjo_pluso@comunidad.unam.mx>
2018-03-21 14:01:28 -0600

    First Controller commit

---
+++ DistribucionDependencias.WebUI/Controllers/ArticuloController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DistribucionDependencias.Domain.Abstract;
+using DistribucionDependencias.Domain.Entities;
+
+namespace DistribucionDependencias.WebUI.Controllers
+{
+    public class ArticuloController : Controller
+    {
+        private IArticuloRepository repository;
+
+        public ArticuloController(IArticuloRepository articuloRepository)
+        {
+            this.repository = articuloRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Articulos);
+        }
+    }
+}
